1be9e95871bae60b26db3c3deb0bd39c4550bf70
Johanna Foston <johannafoston25@gmail.com>
2026-03-06 23:19:40 -0400

    Agregado grid del mapa

--- scripts/game.py
+++ scripts/game.py
@@ -2,15 +2,14 @@ import pygame
 
 class Game:
     def __init__(self):
-        self.width = 800
-        self.height = 600
-        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption("Bomberman IA")
         self.clock = pygame.time.Clock()
         self.running = True
         self.tile_size = 40
-        self.rows = self.height // self.tile_size
-        self.cols = self.width // self.tile_size
+        self.rows = 15
+        self.cols = 20
 
     def run(self):
         while self.running:
@@ -21,6 +20,15 @@ class Game:
                     self.running = False
 
             self.screen.fill((30, 30, 30))
+            for row in range(self.rows):
+             for col in range(self.cols):
+                rect = pygame.Rect(
+                    col * self.tile_size,
+                    row * self.tile_size,
+                    self.tile_size,
+                    self.tile_size
+                )
+                pygame.draw.rect(self.screen, (60, 60, 60), rect, 1)
             pygame.display.flip()
             self.clock.tick(60)
 
